73f8ed7b5ccc058173a45a0039a18daf7def606b
tariq <tariq.mca06@gmail.com>
2013-08-31 00:29:20 +0530

    HoliDayInfo model

---
+++ USTAS/src/com/ustas/db/model/HolidayInfo.java
@@ -0,0 +1,115 @@
+package com.ustas.db.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="holiday_master")
+public class HolidayInfo implements Serializable {   
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="INDEX_NO")
+	private int indexNo;   
+	
+	@Column(name="HOLIDAY_TYPE")
+	private String holidayType;
+	
+	@Column(name="HOLIDAY_NAME")
+	private String holidayName;
+	
+	@Column(name="HOLIDAY_DES")
+	private String holidayDesc;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="FROM_DATE")
+	private Date fromDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="FROM_DATE")
+	private Date toDate;
+	
+	@Column(name="IS_ACTIVE")
+	private boolean isActive;
+	
+	@Column(name="REMARKS")
+	private String remarks;
+
+	public int getIndexNo() {
+		return indexNo;
+	}
+
+	public void setIndexNo(int indexNo) {
+		this.indexNo = indexNo;
+	}
+
+	public String getHolidayType() {
+		return holidayType;
+	}
+
+	public void setHolidayType(String holidayType) {
+		this.holidayType = holidayType;
+	}
+
+	public String getHolidayName() {
+		return holidayName;
+	}
+
+	public void setHolidayName(String holidayName) {
+		this.holidayName = holidayName;
+	}
+
+	public String getHolidayDesc() {
+		return holidayDesc;
+	}
+
+	public void setHolidayDesc(String holidayDesc) {
+		this.holidayDesc = holidayDesc;
+	}
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
+	
+	
+
+}
